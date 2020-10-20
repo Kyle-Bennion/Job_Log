37cbe74535207c966d19db1e1738e8efbbe2d9a6
Kyle Bennion <bilekennion@hotmail.com>
2020-10-19 22:18:43 -0600

    CRUD complete for jobs, Contractor Files Est

---
+++ Setup.sql
@@ -0,0 +1,11 @@
+-- -- CREATE TABLE jobs
+-- -- (
+-- -- id INT NOT NULL AUTO_INCREMENT,
+-- -- description VARCHAR(255),
+-- -- location VARCHAR(255)NOT NULL,
+-- -- contact VARCHAR(255)NOT NULL,
+
+-- -- PRIMARY KEY (id)
+-- -- );
+
+-- SELECT * FROM jobs;
