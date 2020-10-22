2818828449c9e9760a0cce5132b6244a2a3cf9ee
Kyle Bennion <bilekennion@hotmail.com>
2020-10-21 18:12:36 -0600

    Reviews Controller Complete Ers resolved Profile

--- Controllers/ReviewsController.cs
+++ Controllers/ReviewsController.cs
@@ -6,7 +6,7 @@ using CodeWorks.Auth0Provider;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
-namespace JobLog.Controllers
+namespace AmaZen.Controllers
 {
   [ApiController]
   [Route("api/[controller]")]
@@ -20,7 +20,7 @@ namespace JobLog.Controllers
     }
 
     [HttpGet("{id}")]
-    public ActionResult<Review> GetAction(int id)
+    public ActionResult<Review> Get(int id)
     {
       try
       {
@@ -29,55 +29,60 @@ namespace JobLog.Controllers
       catch (AccessViolationException e)
       {
         return Forbid(e.Message);
-        {
-      catch (Exception e)
       }
-      return BadRequest(e.Message);
+      catch (Exception e)
+      {
+        return BadRequest(e.Message);
+      }
     }
-  }
-  [HttpPost]
-  [Authorize]
-  public async Task<ActionResult<Review>> Create([FromBody] Review newReview)
-  {
-    try
-    {
-      Profile userInfo = await HttpContext.GetUserInfoAsync<Profile>();
-      newReview.CreatorId = userInfo.Id;
-      return Ok(_service.Create(newReview));
-    }
-    catch (Exception e)
-    {
-      return BadRequest(e.Message);
-    }
-  }
 
-  [HttpPut("{id}")]
-  public async Task<ActionResult<Review>> Edit([FromBody] Review update, int id)
-  {
-    try
+
+    [HttpPost]
+    [Authorize]
+    public async Task<ActionResult<Review>> Create([FromBody] Review newReview)
     {
-      Profile userInfo = await HttpContext.GetUserInfoAsync<Profile>();
-      update.CreatorId = userInfo.Id;
-      update.Id = id;
+      try
+      {
+        Profile userInfo = await HttpContext.GetUserInfoAsync<Profile>();
+        newReview.CreatorId = userInfo.Id;
+        return Ok(_service.Create(newReview));
+      }
+      catch (Exception e)
+      {
+        return BadRequest(e.Message);
+      }
     }
-    catch (Exception e)
+
+
+    [HttpPut("{id}")]
+    public async Task<ActionResult<Review>> Edit([FromBody] Review update, int id)
     {
-      return BadRequest(e.Message)
+      try
+      {
+        Profile userInfo = await HttpContext.GetUserInfoAsync<Profile>();
+        update.CreatorId = userInfo.Id;
+        update.Id = id;
+        return Ok(_service.Edit(update));
+      }
+      catch (Exception e)
+      {
+        return BadRequest(e.Message);
+      }
+    }
+
+    [HttpDelete("{id}")]
+    [Authorize]
+    public async Task<ActionResult<String>> Delete(int id)
+    {
+      try
+      {
+        Profile userInfo = await HttpContext.GetUserInfoAsync<Profile>();
+        return Ok(_service.Delete(id, userInfo.Id));
+      }
+      catch (Exception e)
+      {
+        return BadRequest(e.Message);
+      }
     }
   }
-  [HttpDelete("{id}")]
-  [Authorize]
-  public async Task<ActionResult<string>> Delete(int id)
-  {
-    try
-    {
-      CacheProfile userInfo = await HttpContext.GetUserInfoAsync<Profile>();
-      return Ok(_service.Delete(id, userInfo.Id));
-    }
-    catch (Exception e)
-    {
-      return BadRequest(e.Message);
-    }
-  }
-}
 }
